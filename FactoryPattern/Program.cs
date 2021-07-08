b105bcd33eb9fe736ede35994e2c74849a22abf6
Muhammad Razif Rizqullah <razifrizqullah@gmail.com>
2021-07-08 14:03:53 +0700

    modify some access modifier

--- FactoryPattern/Program.cs
+++ FactoryPattern/Program.cs
@@ -3,19 +3,30 @@ using CoffeeFactory.BusinessModel;
 using CoffeeFactory.Models;
 
 namespace CoffeeFactory {
+  class Run {
+    private ICoffeeIngredientFactory _factory;
+
+    internal Run() {
+     _factory = new CoffeeIngredientFactory();  
+    }
+
+    internal void Bandung() {
+      ICoffeeStore bdStore = new BandungStore(_factory);
+      ICoffee coffee = bdStore.OrderCoffee("Dark Roast");
+      Console.WriteLine("Here your order is ready: " + coffee.GetName());
+    }
+    internal void Semarang() {
+      ICoffeeStore smStore = new SemarangStore(_factory);
+      ICoffee smCoffee = smStore.OrderCoffee("House Blend");
+      Console.WriteLine("Here your order is ready: " + smCoffee.GetName());
+    }
+  }
   class Program {
     static void Main(string[] args) {
       Console.WriteLine("Coffee - Factory Pattern!");
-      ICoffeeIngredientFactory factory = new CoffeeIngredientFactory();
-
-      ICoffeeStore bdStore = new BandungStore(factory);
-
-      ICoffee coffee = bdStore.OrderCoffee("Dark Roast");
-      Console.WriteLine("Here your order is ready: " + coffee.GetName());
-
-      ICoffeeStore smStore = new SemarangStore(factory);
-      ICoffee smCoffee = smStore.OrderCoffee("House Blend");
-      Console.WriteLine("Here your order is ready: " + smCoffee.GetName());
+      Run store = new Run();
+      store.Bandung();
+      store.Semarang();
     }
   }
 }
